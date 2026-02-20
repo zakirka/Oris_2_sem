0fd71947a545703b6fa0fd03d5076f9e2f58b416
zakirka <164258802+zakirka@users.noreply.github.com>
2026-02-21 01:49:23 +0300

    add homework_1

---
+++ 1_HomeWork/src/main/java/ru/kpfu/itis/entity/User.java
@@ -0,0 +1,35 @@
+package ru.kpfu.itis.entity;
+
+public class User {
+    Long id;
+    String name;
+
+    public User() {}
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
